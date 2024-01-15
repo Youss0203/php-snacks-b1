996b03e13c32500c89b7686dc9315bbad8d53789
Youss0203 <youssefhalibi003@gmail.com>
2024-01-15 12:36:09 +0100

    iniziato a stampare a pagina

--- index.php
+++ index.php
@@ -1,9 +1,4 @@
-/*Creiamo un array contenente le partite di basket di un'ipotetica tappa del calendario.
- Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di 
- casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte 
- le partite con questo schema:
 
-Olimpia Milano - Cantù | 55-60/*
 
 
 <?php
@@ -28,7 +23,6 @@ $partite=[
         'punti2' => '112',
     ],
 ];
-var_dump($partite)
 
-
+print $partite[0]['squadra1'] . print $partite[0]['squadra']
 ?>
