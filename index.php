39496a0d44a1cad855e0c0a8215412b31ed6afb6
Youss0203 <youssefhalibi003@gmail.com>
2024-01-15 12:22:21 +0100

    creato array

---
+++ index.php
@@ -0,0 +1,34 @@
+/*Creiamo un array contenente le partite di basket di un'ipotetica tappa del calendario.
+ Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di 
+ casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte 
+ le partite con questo schema:
+
+Olimpia Milano - Cantù | 55-60/*
+
+
+<?php
+
+$partite=[
+    [
+        'squadra1' => 'Olimpia Milano',
+        'squadra'  =>'Baskonia',
+        'punti1' => '78',
+        'punti2' => '75',
+    ],
+    [
+        'squadra1' => 'Fenerbache',
+        'squadra'  => 'Real Madrid',
+        'punti1' => '94',
+        'punti2' => '108',
+    ],
+    [
+        'squadra1' => 'Olympiacos',
+        'squadra'  => 'Stella Rossa',
+        'punti1' => '104',
+        'punti2' => '112',
+    ],
+];
+var_dump($partite)
+
+
+?>
