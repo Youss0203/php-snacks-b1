b6cb8dccb93106674a88f208282d4a3d1c38ca59
Youss0203 <youssefhalibi003@gmail.com>
2024-01-25 12:42:45 +0100

    creazione classe pokemon

---
+++ OOP/Classes/Pokemon.php
@@ -0,0 +1,14 @@
+<?php
+
+class Pokemon{
+    public $nome;
+    public $potere;
+    public $vita;
+}
+
+
+$Pippo = new Pokemon()
+$Pippo->nome="Pippo";
+$Pippo->potere="Fuoco";
+$Pippo->vita=100;
+
---
+++ OOP/index.php
@@ -0,0 +1,2 @@
+Create una nuova classe che rappresenti un Pokemon e che abbia almeno cinque proprieta, 
+tre metodi ed il costruttore.
